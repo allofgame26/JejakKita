e5c720d3d4e0d6818201b37d691e3f924b60bb20
Mochammad Dicky Hanun Prasetyo <dickyhanunprasetyo@gmail.com>
2025-08-13 15:29:27 +0700

    Pengerjaan Fitur Pengisian Donasi

--- app/Filament/Resources/TransaksiDonasiProgramResource/Pages/EditTransaksiDonasiProgram.php
+++ app/Filament/Resources/TransaksiDonasiProgramResource/Pages/EditTransaksiDonasiProgram.php
@@ -48,7 +48,7 @@ class EditTransaksiDonasiProgram extends EditRecord
                 SpatieMediaLibraryFileUpload::make('bukti_pembayaran')
                     ->label('Bukti Pembayaran')
                     ->collection('bukti_pembayaran')
-                    ->image()->imageEditor(),
+                    ->image()->imageEditor()
                     ->reactive()
                     ->required()
                     ->image()->imageEditor()
