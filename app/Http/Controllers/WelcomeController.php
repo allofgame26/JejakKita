232cd37cda381501906ece96c415ddcae99cb283
allofgame26 <rizkiarya79@yahoo.com>
2025-09-23 15:50:28 +0700

    Debug Web Profile

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\m_kategori;
+use App\Models\m_program_pembangunan;
 use App\Policies\m_kategoriPolicy;
 use Illuminate\Http\Request;
 
@@ -12,8 +13,21 @@ class WelcomeController extends Controller
     {
         $semuaKategori = m_kategori::has('posts')->with('posts.media')->orderBy('row','asc')->get();
 
+        $programDonasi = m_program_pembangunan::where('status_pendanaan', '!=' ,'lengkap')->orderBy('skor_prioritas_akhir', 'desc')->limit(3)->get()->map(function ($program){
+            $terkumpul = $program->hitungTotalDonasiTerkumpul();
+            $target = $program->estimasi_biaya;
+
+            $persentase = ($target > 0 ) ? ($terkumpul / $target)* 100 : 0;
+
+            $program->dana_terkumpul = $terkumpul;
+            $program->persentase_terkumpul = $persentase;
+
+            return $program;
+        });
+
         return view('welcomes', [
             'daftarKategori' => $semuaKategori,
+            'programDonasi' => $programDonasi,
         ]);
     }
 }
---
+++ config/api-service.php
@@ -0,0 +1,16 @@
+<?php
+
+// config for Rupadana/ApiService
+return [
+    'navigation' => [
+        'group' => [
+            'token' => 'User',
+        ],
+    ],
+    'models' => [
+        'token' => [
+            'enable_policy' => true,
+        ],
+    ],
+    'auth_guard' => 'sanctum',
+];
